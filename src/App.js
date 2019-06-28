27c20cb5e5272a3d265465fe053fa6f04051e730
Angie <angievarela.silva@gmail.com>
2019-06-28 13:13:24 +0200

    second comit

--- src/App.js
+++ src/App.js
@@ -4,11 +4,13 @@ import Recipe from "../src/Recipe";
 
 
 
+
 const App = () => {
 
   const APP_ID = "00ef6e28";
   const APP_KEY = "5dad1fce1767009d36985eeb88b57b2f";
-  const url = `https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`;
+  const [query, setQuery] = useState('chicken');
+  const url = `https://api.edamam.com/search?q=${query}&app_id=${APP_ID}&app_key=${APP_KEY}`;
 
   const [recipes, setRecipes] = useState([]);
   const [search, setSearch] = useState("");
@@ -17,9 +19,10 @@ const App = () => {
 
     useEffect(() => {
       getRecipes();
-    }, []);
+    }, [query]);
     // c'est le second argument de la fonction useEffect
-    // afin que la page ne charge qu'une fois cette fonction
+    // afin que la page ne charge qu'une fois cette fonction = refresh à l'acivation du submit
+    // Avoir des recettes à chaque fois que l'on tape un aliment dans l'input
 
 
     // Création du fetch afin de récupérer les données
@@ -31,26 +34,49 @@ const App = () => {
       console.log(data.hits);
       setRecipes(data.hits);
     }
+    
 
+    // event  des l'activation de la fonction Onchange, on aura accès à l'event qui sera activer.
+    // value = la valeur de l'input
+    // setSearch = setState = il change le state par default
+    const updateSearch = e => {
+      setSearch(e.target.value);
+      console.log('input:', search);
+    }
+
+
+
+    // activation de la fonction dés le clique sur submit
+    // e.preventDefault = éviter le rafraichissement de la page
+    const getSearch = e => {
+      e.preventDefault();
+      setQuery(search);
+      setSearch('');
+    }
+
+  
 
     return (
       <div className="App">
         <h1> Miamiiz </h1>
-        <form className="search-form">
-          <input className="search-bar" type="text" value={search} />
+        <form onSubmit={getSearch} className="search-form">
+          <input className="search-bar" type="text" value={search} onChange={updateSearch}/>
           <button className="search-button" type="submit"> 
           Search
           </button>
         </form>
-        {/* creation de la map qui affichera les informations d'une carte de recette*/}
-        {recipes.map(recipe => (
-          <Recipe 
-          title={recipe.recipe.label}
-          calories={recipe.recipe.calories}
-          image={recipe.recipe.image} 
-          />
-          // Les props
-        ))};
+        <div className="recipes">
+          {/* creation de la map qui affichera les informations d'une carte de recette*/}
+          {recipes.map(recipe => (
+            <Recipe 
+            title={recipe.recipe.label}
+            calories={recipe.recipe.calories}
+            image={recipe.recipe.image} 
+            ingredients={recipe.recipe.ingredients}
+            />
+            // Les props
+          ))}
+        </div>
       </div>
     );
   }
--- src/Recipe.js
+++ src/Recipe.js
@@ -1,12 +1,24 @@
 import React from "react";
+import style from './recipe.module.css';
 
-const Recipe = ({title, calories, image}) => {
+const Recipe = ({title, calories, image, ingredients}) => {
   return(
-    <div>
+    <div className={style.recipe}>
       <h1> {title} </h1>
-      <p> {calories} </p>
-      <img src={image} alt={title}/>
-    </div>
+      <img className={style.image} src={image} alt={title}/>
+      <h5> Calories : {calories} Kcal </h5>
+      
+      <h4> Ingredients </h4>
+      <ul>
+        {ingredients.map(ingredient =>(
+          <li>{ingredient.text}</li>
+        ))}
+      </ul>
+     
+      </div>
+
+  
+
   );
 }
 
